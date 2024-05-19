1e125b0710562220e1629266fea9f0d0b9fb836b
ArnasIc3 <149010264+ArnasIc3@users.noreply.github.com>
2024-05-19 22:54:04 +0300

    Add files via upload

---
+++ doxygen/html/class_studentas.js
@@ -0,0 +1,21 @@
+var class_studentas =
+[
+    [ "Studentas", "class_studentas.html#ab459e995e8c9b24cdc9aec5b09a66539", null ],
+    [ "Studentas", "class_studentas.html#a23db4a343596949e930834957ba73088", null ],
+    [ "Studentas", "class_studentas.html#aef0484fe46cf05746f8ffc0d083fcf3e", null ],
+    [ "Studentas", "class_studentas.html#a732b15e3745fa8e35ff0002cfc4b73b5", null ],
+    [ "~Studentas", "class_studentas.html#a63e449e0f51a0b14340d02ee71b4be23", null ],
+    [ "calculateGalutinis", "class_studentas.html#a8a3ed42df5c3238fa509310e8ebacd91", null ],
+    [ "getEgzaminas", "class_studentas.html#ab196f6bc083df87981c65804ce73dde1", null ],
+    [ "getGalutinis", "class_studentas.html#adff52a10ba657a5c8854693988082820", null ],
+    [ "getND", "class_studentas.html#a72ad9cbcac7d53cefe587120d379a78b", null ],
+    [ "getPavarde", "class_studentas.html#afc1c025f58544eed43273afa98445853", null ],
+    [ "getVardas", "class_studentas.html#a4471c08cbf44faf5fe5f4c192922bf1b", null ],
+    [ "operator=", "class_studentas.html#a25bcc531503d3bc490dab4a656e3b8e9", null ],
+    [ "operator=", "class_studentas.html#a6379482d741209e9a2d902023676fb45", null ],
+    [ "setEgzaminas", "class_studentas.html#a305a5fcb7c109eb624ba8ac538b9d7de", null ],
+    [ "setGalutinis", "class_studentas.html#afe8208d99f1c1fefabb9758a5061bd3e", null ],
+    [ "setNd", "class_studentas.html#a695bc9626776c845a6d6203401749fa6", null ],
+    [ "setPavarde", "class_studentas.html#a7f34ab90810686b13945d56c8c29b2be", null ],
+    [ "setVardas", "class_studentas.html#ada6e133cee4347851493cc4b55219838", null ]
+];
---
+++ doxygen/html/class_zmogus.js
@@ -0,0 +1,9 @@
+var class_zmogus =
+[
+    [ "~Zmogus", "class_zmogus.html#a6183937c7da8254430a6c99cfa89fe69", null ],
+    [ "getEgzaminas", "class_zmogus.html#a6ed8ce384b97ad01fc951b5d4fb29ea9", null ],
+    [ "getGalutinis", "class_zmogus.html#aac2bc58f772db6da873c91e57d896530", null ],
+    [ "getND", "class_zmogus.html#a9546e4c1df5deb12826e1f561e6e15c5", null ],
+    [ "getPavarde", "class_zmogus.html#ab7a875d196b6224edf8280829193e410", null ],
+    [ "getVardas", "class_zmogus.html#a34858b36a8082c3ce73c3230b3f3530d", null ]
+];
